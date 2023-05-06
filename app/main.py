2301d2159c9c2c477bdded7a26a90d63be1521b2
Paweł Uchański <uchanski.pawel@gmail.com>
2023-05-06 11:47:56 +0200

    feat: add opencv and endpoint for face counter from image

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+import datetime
+import cv2
+import numpy as np
+
+app = FastAPI()
+
+
+@app.get("/date")
+async def get_date():
+    return {"date": str(datetime.datetime.now())}
+
+@app.post("/faces/")
+async def count_faces(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
+    return {"num_faces": len(faces)}
